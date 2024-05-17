bbf147ce4282d092d048cad2c2927597670585b6
PennyYoon <55304072+Chenxulin97@users.noreply.github.com>
2024-05-17 11:06:59 +0800

    Update init-bookkeeper.sh

--- docker-build/scripts/init-bookkeeper.sh
+++ docker-build/scripts/init-bookkeeper.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-$BOOKKEEPER_HOME/bin/bookkeeper shell metaformat
+$BOOKKEEPER_HOME/bin/bookkeeper shell bookieformat -deleteCookie -n -f
+
+$BOOKKEEPER_HOME/bin/bookkeeper shell metaformat -n -f
+
+$BOOKKEEPER_HOME/bin/bookkeeper bookie
